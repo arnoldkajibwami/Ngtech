4643edc4ab129649670fa880201dab79b50180d6
Arnold kajibwami <arnoldkajinguma8@gmail.com>
2023-10-12 15:03:20 +0200

    set a countedown timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,51 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
-import NavbarHeader from './NavbarHeader'
-import Home from './Home';
-import About from './About';
-import Gallery from './Gallery';
-import Service from './Service';
-import Contact from './Contact'
-import Posts from './Posts';
+import React from 'react';
+import Image1 from './images/2.png'
+import { useTimer } from 'react-timer-hook';
 
+function MyTimer({ expiryTimestamp }) {
+  const {
+    totalSeconds,
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    resume,
+    restart,
+  } = useTimer({ expiryTimestamp, onExpire: () => console.warn('onExpire called') });
 
-function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Router>
-        <NavbarHeader />
-        {/* <Home/> */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/post" element={<Posts />} />
-        </Routes>
-      </Router>
-    </>
-  )
+    <div style={{textAlign: 'center'}}>
+      <h1>Comming Soon</h1>
+      <p>Ng Tech</p>
+      <img id="imagebg" src= {Image1} alt='logo'/>
+      <div style={{fontSize: '80px'}}>
+        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:
+        <span>{seconds}</span>
+      </div>
+      {/* <p>{isRunning ? 'Running' : 'Not running'}</p> */}
+      {/* <button onClick={start}>Start</button>
+      <button onClick={pause}>Pause</button>
+      <button onClick={resume}>Resume</button> */}
+      {/* <button onClick={() => {
+        // Restarts to 5 minutes timer
+        const time = new Date();
+        time.setSeconds(time.getSeconds() + 300);
+        restart(time)
+      }}>Restart</button> */}
+    </div>
+  );
 }
 
-export default App
-
+export default function App() {
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 5250000); // 90 days timer
+  return (
+    <div style={{marginTop:"150px"}}>
+      <MyTimer expiryTimestamp={time} />
+    </div>
+  );
+}
